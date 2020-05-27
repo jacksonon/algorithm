5580a46b41df38730a3d6e08bd85e98a8cfe09f3
LightYear <542413041@qq.com>
2020-05-27 18:36:13 +0800

    Create 对称二叉树.cpp

---
+++ 对称二叉树.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack> // 栈使用
+#include <unordered_map>
+#include <vector> // 向量容器
+#include <array>
+#include <cctype>
+#include <string>
+#include<algorithm>
+using namespace std;
+
+
+// 定义二叉树节点
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// 对称二叉树
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        isMirror(root, root);
+    }
+
+    bool isMirror(TreeNode* t1, TreeNode* t2) {
+        if (t1 == NULL && t2 == NULL) {
+            return true;
+        }
+
+        if (t1 == NULL || t2 == NULL) {
+            return false;
+        }
+
+        return (t1->val == t2->val) && (isMirror(t1->right, t2->left)) && (isMirror(t1->left, t2->right));
+    }
+};
+
+int main() {
+    return 0;
+}
