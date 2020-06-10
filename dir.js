6ede6bd9c468b18f7f867407a8e762661de9c681
wangjiawei601996 <wangjiawei601996@pwrd.com>
2020-06-10 18:17:05 +0800

    hh

--- dir.js
+++ dir.js
@@ -22,7 +22,7 @@ fs.readdir("./", function(err, files) {
 	    count += 1;
         }
     });
-    var days = Math.floor(((Date('2020/05/19 0:0:0').getTime()-Date().getTime())/(24 * 3600 * 1000)));
+    var days = Math.floor((Date('2020/05/19 0:0:0').getTime()-Date().getTime())/(24 * 3600 * 1000));
     var tims = '\n > 总数 : ' + count + '题' + '\n' + '累计答题：' + days + '天';
     fs.appendFileSync(fileName, tims);
 });
